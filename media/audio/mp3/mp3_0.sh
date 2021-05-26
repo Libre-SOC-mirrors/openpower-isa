f2daa87fd5f993de29089980aa2bc723db59f158
Lauri Kasanen <cand@gmx.com>
2021-05-26 11:49:03 +0300

    Dedup them via scripts

---
+++ media/audio/mp3/mp3_0.sh
@@ -0,0 +1,9 @@
+#!/bin/sh -e
+
+pypowersim -g audio/mp3/mp3_0.gpr \
+	-s common.spr \
+	-l data/audio/mp3/mp3_0_data/buf${1}:0x100000 \
+	-l data/audio/mp3/mp3_0_data/win0:0x200000 \
+	-d ${DUMP}:0x400000:128 \
+	-i audio/mp3/mp3_0_apply_window_float.bin
+cmp ${DUMP} data/audio/mp3/mp3_0_data/samples${1}
---
+++ media/audio/mp3/mp3_1.sh
@@ -0,0 +1,11 @@
+#!/bin/sh -e
+
+pypowersim -g audio/mp3/mp3_1.gpr \
+	-s common.spr \
+	-l data/audio/mp3/mp3_1_data/beforeout${1}:0x100000 \
+	-l data/audio/mp3/mp3_1_data/buf${1}:0x200000 \
+	-l data/audio/mp3/mp3_1_data/in${1}:0x300000 \
+	-l data/audio/mp3/mp3_1_data/win${1}:0x400000 \
+	-d ${DUMP}:0x100000:2304 \
+	-i audio/mp3/mp3_1_imdct36_float.bin
+cmp ${DUMP} data/audio/mp3/mp3_1_data/out${1}
