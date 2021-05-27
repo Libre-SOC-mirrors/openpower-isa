07314a214f5084edc84d36aae5c249a46f5057f1
Luke Kenneth Casson Leighton <lkcl@lkcl.net>
2021-05-27 12:24:13 +0100

    pass output dump argument to shell script for audio tests

--- media/audio/mp3/mp3_0.sh
+++ media/audio/mp3/mp3_0.sh
@@ -4,6 +4,6 @@ pypowersim -g audio/mp3/mp3_0.gpr \
 	-s common.spr \
 	-l data/audio/mp3/mp3_0_data/buf${1}:0x100000 \
 	-l data/audio/mp3/mp3_0_data/win0:0x200000 \
-	-d ${DUMP}:0x400000:128 \
+	-d ${2}:0x400000:128 \
 	-i audio/mp3/mp3_0_apply_window_float.bin
-cmp ${DUMP} data/audio/mp3/mp3_0_data/samples${1}
+cmp ${2} data/audio/mp3/mp3_0_data/samples${1}
--- media/audio/mp3/mp3_1.sh
+++ media/audio/mp3/mp3_1.sh
@@ -6,6 +6,6 @@ pypowersim -g audio/mp3/mp3_1.gpr \
 	-l data/audio/mp3/mp3_1_data/buf${1}:0x200000 \
 	-l data/audio/mp3/mp3_1_data/in${1}:0x300000 \
 	-l data/audio/mp3/mp3_1_data/win${1}:0x400000 \
-	-d ${DUMP}:0x100000:2304 \
+	-d ${2}:0x100000:2304 \
 	-i audio/mp3/mp3_1_imdct36_float.bin
-cmp ${DUMP} data/audio/mp3/mp3_1_data/out${1}
+cmp ${2} data/audio/mp3/mp3_1_data/out${1}
