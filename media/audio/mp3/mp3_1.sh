de41d113511536a889a8ef18e8dde1da0bd9f366
Lauri Kasanen <cand@gmx.com>
2021-05-28 14:42:25 +0300

    Correct mp3_1 dump size

--- media/audio/mp3/mp3_1.sh
+++ media/audio/mp3/mp3_1.sh
@@ -7,6 +7,6 @@ pypowersim -g audio/mp3/mp3_1.gpr \
 	-l data/audio/mp3/mp3_1_data/buf${1}:0x200000 \
 	-l data/audio/mp3/mp3_1_data/in${1}:0x300000 \
 	-l data/audio/mp3/mp3_1_data/win${1}:0x400000 \
-	-d ${2}:0x100000:2304 \
+	-d ${2}:0x100000:9216 \
 	-i audio/mp3/mp3_1_imdct36_float.bin
 cmp ${2} data/audio/mp3/mp3_1_data/out${1}
