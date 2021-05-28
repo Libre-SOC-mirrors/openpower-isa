f72792bee152dc62cca569b4e6f07103ba1eb47b
Lauri Kasanen <cand@gmx.com>
2021-05-28 14:41:08 +0300

    Undo qemu address changes

--- media/audio/mp3/mp3_0.sh
+++ media/audio/mp3/mp3_0.sh
@@ -2,9 +2,9 @@
 
 pypowersim -g audio/mp3/mp3_0.gpr \
 	-s common.spr \
-    -p 0x20000000 \
-	-l data/audio/mp3/mp3_0_data/buf${1}:0x20100000 \
-	-l data/audio/mp3/mp3_0_data/win0:0x20200000 \
-	-d ${2}:0x20400000:128 \
+	-p 0x20000000 \
+	-l data/audio/mp3/mp3_0_data/buf${1}:0x100000 \
+	-l data/audio/mp3/mp3_0_data/win0:0x200000 \
+	-d ${2}:0x400000:128 \
 	-i audio/mp3/mp3_0_apply_window_float.bin
 cmp ${2} data/audio/mp3/mp3_0_data/samples${1}
