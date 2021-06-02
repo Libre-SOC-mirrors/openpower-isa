b04990f79c3298d68f4a0ca86c3d01091cd204f6
Luke Kenneth Casson Leighton <lkcl@lkcl.net>
2021-06-02 13:35:55 +0100

    move mp3 test params slightly higher up so as not to clash with qemu BIOS

--- media/audio/mp3/mp3_0.sh
+++ media/audio/mp3/mp3_0.sh
@@ -3,8 +3,8 @@
 pypowersim -g audio/mp3/mp3_0.gpr \
 	-s common.spr \
 	-p 0x20000000 \
-	-l data/audio/mp3/mp3_0_data/buf${1}:0x100000 \
-	-l data/audio/mp3/mp3_0_data/win0:0x200000 \
-	-d ${2}:0x400000:128 \
+	-l data/audio/mp3/mp3_0_data/buf${1}:0x600000 \
+	-l data/audio/mp3/mp3_0_data/win0:0x700000 \
+	-d ${2}:0x900000:128 \
 	-i audio/mp3/mp3_0_apply_window_float.bin
 cmp ${2} data/audio/mp3/mp3_0_data/samples${1}
--- media/audio/mp3/mp3_1.sh
+++ media/audio/mp3/mp3_1.sh
@@ -3,10 +3,10 @@
 pypowersim -g audio/mp3/mp3_1.gpr \
 	-s common.spr \
 	-p 0x20000000 \
-	-l data/audio/mp3/mp3_1_data/beforeout${1}:0x100000 \
-	-l data/audio/mp3/mp3_1_data/buf${1}:0x200000 \
-	-l data/audio/mp3/mp3_1_data/in${1}:0x300000 \
-	-l data/audio/mp3/mp3_1_data/win${1}:0x400000 \
-	-d ${2}:0x100000:9216 \
+	-l data/audio/mp3/mp3_1_data/beforeout${1}:0x600000 \
+	-l data/audio/mp3/mp3_1_data/buf${1}:0x700000 \
+	-l data/audio/mp3/mp3_1_data/in${1}:0x800000 \
+	-l data/audio/mp3/mp3_1_data/win${1}:0x900000 \
+	-d ${2}:0x600000:9216 \
 	-i audio/mp3/mp3_1_imdct36_float.bin
 cmp ${2} data/audio/mp3/mp3_1_data/out${1}
