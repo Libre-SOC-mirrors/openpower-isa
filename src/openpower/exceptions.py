958cb90a00a45a4ee756b3cab323eb64cb4baf72
Luke Kenneth Casson Leighton <lkcl@lkcl.net>
2021-12-03 17:32:00 +0000

    add a namedtuple LDSTExceptionTuple which allows obtaining list of all the exception types in LDSTException

--- src/openpower/exceptions.py
+++ src/openpower/exceptions.py
@@ -2,12 +2,16 @@
 """
 from nmutil.iocontrol import RecordObject
 from nmigen import Signal
+from collections import namedtuple
+
+exc_types = ['alignment', 'instr_fault', 'invalid', 'badtree',
+             'perm_error', 'rc_error', 'segment_fault',
+              'happened', ] # must be last: may overlap with Data.ok
+LDSTExceptionTuple = namedtuple("LDSTExceptionTuple", exc_types)
 
 # https://bugs.libre-soc.org/show_bug.cgi?id=465
 class LDSTException(RecordObject):
-    _exc_types = ['alignment', 'instr_fault', 'invalid', 'badtree',
-                 'perm_error', 'rc_error', 'segment_fault',
-                  'happened', ] # must be last: may overlap with Data.ok
+    _exc_types = exc_types
     def __init__(self, name=None):
         RecordObject.__init__(self, name=name)
         for f in self._exc_types:
