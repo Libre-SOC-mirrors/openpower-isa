e1ab56314eb96e7da1e388d75ae3df44fab1fd31
Jacob Lifshay <programmerjake@gmail.com>
2022-09-02 18:50:01 -0700

    fix test_caller_svshape2.py

--- src/openpower/decoder/isa/test_caller_svshape2.py
+++ src/openpower/decoder/isa/test_caller_svshape2.py
@@ -81,7 +81,8 @@ class SVSTATETestCase(FHDLTestCase):
             self.assertEqual(SVSHAPE0.xdimsz, 5)  # set
             self.assertEqual(SVSHAPE0.ydimsz, 2)  # calculated from MVL/xdimsz
             self.assertEqual(SVSHAPE0.skip, 0)   # no skip
-            self.assertEqual(SVSHAPE0.invxyz, 0)  # (no inversion possible)
+            # (no inversion possible)
+            self.assertEqual(SVSHAPE0.invxyz, [0, 0, 0])
             self.assertEqual(SVSHAPE0.offset, 12)
             self.assertEqual(SVSHAPE0.order, (1, 0, 2))  # y,x(,z)
             self.assertEqual(sim.svstate.RMpst, 0)  # mm=0 so persist=0
