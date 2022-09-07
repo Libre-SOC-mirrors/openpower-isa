24c82e027fd4bfa3ff0c811407a4f9731365b03a
Luke Kenneth Casson Leighton <lkcl@lkcl.net>
2022-09-07 19:40:01 +0100

    add 2nd parallel prefix test, this time subtract (non-commutative)

--- src/openpower/decoder/isa/test_caller_svp64_parallel_reduce.py
+++ src/openpower/decoder/isa/test_caller_svp64_parallel_reduce.py
@@ -26,12 +26,12 @@ class DecoderTestCase(FHDLTestCase):
         for i in range(32):
             self.assertEqual(sim.gpr(i), SelectableInt(expected[i], 64))
 
-    def test_sv_remap1(self):
+    def tst_sv_remap1(self):
         """>>> lst = ["svshape 7, 0, 0, 7, 0",
                         "svremap 31, 0, 1, 0, 0, 0, 0",
                        "sv.add *0, *8, *16"
                         ]
-                REMAP fmadds FRT, FRA, FRC, FRB
+                REMAP add RT,RA,RB
         """
         lst = SVP64Asm(["svshape 7, 0, 0, 7, 0",
                         "svremap 31, 0, 1, 0, 0, 0, 0",
@@ -69,6 +69,51 @@ class DecoderTestCase(FHDLTestCase):
             for i, v in enumerate(res):
                 self.assertEqual(v, expected[i])
 
+
+    def test_sv_remap2(self):
+        """>>> lst = ["svshape 7, 0, 0, 7, 0",
+                        "svremap 31, 1, 0, 0, 0, 0, 0", # different order
+                       "sv.subf *0, *8, *16"
+                        ]
+                REMAP sv.subf RT,RA,RB - inverted application of RA/RB
+                                         left/right due to subf
+        """
+        lst = SVP64Asm(["svshape 7, 0, 0, 7, 0",
+                        "svremap 31, 1, 0, 0, 0, 0, 0",
+                       "sv.subf *0, *0, *0"
+                        ])
+        lst = list(lst)
+
+        gprs = [0] * 64
+        vec = [1, 2, 3, 4, 9, 5, 6]
+
+        # and create a linear result2, same scheme
+        #result1 = [0] * (ydim1*xdim2)
+
+
+        res = []
+        # store GPRs
+        for i, x in enumerate(vec):
+            gprs[i] = x
+
+        with Program(lst, bigendian=False) as program:
+            sim = self.run_tst_program(program, initial_regs=gprs)
+            print ("spr svshape0", sim.spr['SVSHAPE0'])
+            print ("    xdimsz", sim.spr['SVSHAPE0'].xdimsz)
+            print ("    ydimsz", sim.spr['SVSHAPE0'].ydimsz)
+            print ("    zdimsz", sim.spr['SVSHAPE0'].zdimsz)
+            print ("spr svshape1", sim.spr['SVSHAPE1'])
+            print ("spr svshape2", sim.spr['SVSHAPE2'])
+            print ("spr svshape3", sim.spr['SVSHAPE3'])
+            for i in range(7):
+                val = sim.gpr(i).value
+                res.append(val)
+                print ("i", i, val)
+            # confirm that the results are as expected, mask with 64-bit
+            expected = preduce_y(vec, operation=operator.sub)
+            for i, v in enumerate(res):
+                self.assertEqual(v&0xffffffffffffffff,
+                                 expected[i]&0xffffffffffffffff)
 
     def run_tst_program(self, prog, initial_regs=None,
                               svstate=None,
