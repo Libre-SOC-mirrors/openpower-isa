076978196706bfe25f2997aa963937024a8cd504
Konstantinos Margaritis <konstantinos.margaritis@vectorcamp.gr>
2022-09-25 16:54:22 +0000

    add prototypes

--- media/video/libvpx/variance_svp64_wrappers.h
+++ media/video/libvpx/variance_svp64_wrappers.h
@@ -9,3 +9,12 @@ void variance_svp64_real(const uint8_t *src_ptr, int src_stride,
                     const uint8_t *ref_ptr, int ref_stride, int w, int h,
                     uint32_t *sse, int *sum);
 
+uint32_t vpx_get_mb_ss_svp64(const int16_t *src_ptr);
+
+uint32_t vpx_get4x4sse_cs_svp64(const uint8_t *src_ptr, int src_stride,
+                                     const uint8_t *ref_ptr, int ref_stride);
+
+void variance_svp64(const uint8_t *src_ptr, int src_stride,
+                    const uint8_t *ref_ptr, int ref_stride, int w, int h,
+                    uint32_t *sse, int *sum);
+
