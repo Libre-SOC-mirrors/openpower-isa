70498a68baab54982ffaf2300f51d77d21c1b252
Luke Kenneth Casson Leighton <lkcl@lkcl.net>
2022-12-30 13:20:56 +0000

    add misaligned mem test

--- src/openpower/decoder/isa/test_mem.py
+++ src/openpower/decoder/isa/test_mem.py
@@ -9,12 +9,20 @@ from openpower.util import log
 
 class TestMem(unittest.TestCase):
 
-    def test_mem_align_ld(self):
+    def test_mem_align_st(self):
         m = Mem(row_bytes=8, initial_mem={})
         m.st(4, 0x12345678, width=4, swap=False)
         d = m.dump()
         log ("dict", d)
         self.assertEqual(d, [(0, 0x1234567800000000)])
+
+    def test_mem_misalign_st(self):
+        m = Mem(row_bytes=8, initial_mem={})
+        m.st(4, 0x912345678, width=8, swap=False)
+        d = m.dump()
+        log ("dict", d)
+        self.assertEqual(d, [(0, 0x1234567800000000),
+                              8, 0x0000000000000009])
 
 
 if __name__ == '__main__':
