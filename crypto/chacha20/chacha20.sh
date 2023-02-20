17eeb741b2cdc63bce571606dc5a3f508bae64f2
Luke Kenneth Casson Leighton <lkcl@lkcl.net>
2023-02-20 12:44:03 +0000

    after move data to new directory, update runner-script to match

--- crypto/chacha20/chacha20.sh
+++ crypto/chacha20/chacha20.sh
@@ -4,10 +4,10 @@
 pypowersim -g chacha20.gpr \
 	-s common.spr \
 	-p 0x20000000 \
-    -l ./chacha20.key:0x600000 \
-    -l ./chacha20.iv:0x700000 \
-    -l ./chacha20.cipher:0x800000 \
-    -l ./chacha20.plain:0x900000 \
+    -l ./data/chacha20.key:0x600000 \
+    -l ./data/chacha20.iv:0x700000 \
+    -l ./data/chacha20.cipher:0x800000 \
+    -l ./data/chacha20.plain:0x900000 \
     -d ./chacha20.out:0x500000:128 \
 	-i chacha20test.bin
 #cmp ${2} data/audio/mp3/mp3_0_data/samples${1}
