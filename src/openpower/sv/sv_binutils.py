e3ea73ea5f589bbba5063d62279c2230aad6c254
Dmitry Selyutin <ghostmansd@gmail.com>
2023-04-10 19:11:23 +0300

    sv_binutils: fix broken script

--- src/openpower/sv/sv_binutils.py
+++ src/openpower/sv/sv_binutils.py
@@ -664,6 +664,12 @@ def collect(db):
 
             if issubclass(cls, Boolean):
                 value = Boolean(value)
+            elif issubclass(cls, Extra):
+                value = tuple(value)
+                if not value:
+                    value = cls["NONE"]
+                else:
+                    value = cls[value[0].name]
             elif issubclass(cls, _enum.Enum):
                 value = cls[value.name]
             else:
