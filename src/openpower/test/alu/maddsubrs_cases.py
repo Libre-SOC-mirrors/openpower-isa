50ec7431bb89eb3c8cf7e7ae70044e235deebad1
Konstantinos Margaritis <konstantinos.margaritis@vectorcamp.gr>
2023-04-28 16:02:26 +0000

    almost there, positive values work, negative values differ by 1

--- src/openpower/test/alu/maddsubrs_cases.py
+++ src/openpower/test/alu/maddsubrs_cases.py
@@ -13,19 +13,16 @@ import unittest
 class MADDSUBRSTestCase(TestAccumulatorBase):
 
     def case_0_maddsubrs(self):
-        isa = SVP64Asm(["maddsubrs 1,2,3,0"])
+        isa = SVP64Asm(["maddsubrs 1,2,14,3"])
         lst = list(isa)
 
         initial_regs = [0] * 32
         initial_regs[1] = 0x00000a70
-        initial_regs[2] = 0xffffe6b8
+        initial_regs[2] = 0x0000e6b8
         initial_regs[3] = 0x00002d41
 
         e = ExpectedState(pc=4)
-        e.intregs[1] = 0xfffff581
-        e.intregs[11] = 0x00001942
+        e.intregs[1] = 0x0000aa85
+        e.intregs[2] = 0xffffffffffff643e
         self.add_case(Program(lst, bigendian), initial_regs, expected=e)
-
-        self.add_case(Program(lst, bigendian), expected=e)
-
 
