994585f1725654987b73ebdc54db03585b4e46f4
Luke Kenneth Casson Leighton <lkcl@lkcl.net>
2023-10-01 16:05:30 +0100

    whoops should be 5x3 comment not 5x3

--- src/openpower/decoder/isa/test_caller_svp64_matrix.py
+++ src/openpower/decoder/isa/test_caller_svp64_matrix.py
@@ -121,7 +121,7 @@ class DecoderTestCase(FHDLTestCase):
               [4, 5, 6],
               [7, 8, 9],
               [2, 0, 1]]
-        # 3x5 matrix
+        # 5x3 matrix
         Y3 = [[5, 8, 1, 2, 3],
               [6, 7, 3, 0, 9],
               [4, 5, 9, 1, 2]]
